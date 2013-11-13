1b59db60a2c0c68b14668c792c30af36594068c4
gaara <kazekage.kun@gmail.com>
2013-11-13 22:16:26 +0400

    i3pystatus

---
+++ i3pystatus.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+#import subprocess
+
+from . import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+                format="%a %-d %b %X",)
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+                format="CPU:{temp:.0f}°C",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load",
+                format="{avg1} {avg5} {avg15}",)
+
+# Show memory usage
+status.register("mem",
+                format="Memory U:{percent_used_mem}% A:{avail_mem} T:{total_mem}")
+
+# The battery monitor has many formatting options, see README for details
+
+# This would look like this, when discharging (or charging)
+# ↓14.22W 56.15% [77.81%] 2h:41m
+# And like this if full:
+# =14.22W 100.0% [91.21%]
+#
+# This would also display a desktop notification (via dbus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+  # status.register("battery",
+  #     format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+  #     alert=True,
+  #     alert_percentage=5,
+  #     status={
+  #         "DIS": "↓",
+  #         "CHR": "↑",
+  #         "FULL": "=",
+  #     },)
+
+# This would look like this:
+# Discharging 6h:51m
+  # status.register("battery",
+  # format="{status} {remaining_hm}",
+    # alert=True,
+    # alert_percentage=5,
+    # status={
+    #     "DIS":  "Discharging",
+    #     "CHR":  "Charging",
+    #     "FULL": "Bat full",
+    # },)
+
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces-py3
+status.register("network",
+                interface="eth0",
+                format_up="{name}: {v4cidr}",)
+
+# Has all the options of the normal network and adds some wireless specific things
+# like quality and network names.
+#
+# Note: requires both netifaces-py3 and basiciw
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+
+status.register("disk",
+                path="/home/",
+                format="HDD:{used}/{total}G [{avail}G]",)
+
+status.register("disk",
+                path="/",
+                format="SSD:{used}/{total}G [{avail}G]",)
+
+# Shows weather
+status.register("weather",
+                location_code="RSXX0063:1",
+                format="Moscow: {current_temp}")
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+                format="♪: {volume}%",)
+
+#   status.register("mpd",
+#       format="{title}{status}{album}",
+#       status={
+#           "pause": "\u25b7",
+#           "play": "\u25b6",
+#           "stop": "\u25fe",
+#       },)
+
+status.run()
